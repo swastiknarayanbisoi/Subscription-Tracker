a13fe8ce6d12354b71c8ef488e71fde46a48742b
Swastik <swastikbisoi08@gmail.com>
2026-04-10 22:55:27 +0530

    added firebase details to .env

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCIjwIpqIBVp6AEjycW77hgSA9fi-DrzVg",
-  authDomain: "subscription-tracker-80f2d.firebaseapp.com",
-  projectId: "subscription-tracker-80f2d",
-  storageBucket: "subscription-tracker-80f2d.firebasestorage.app",
-  messagingSenderId: "585170723682",
-  appId: "1:585170723682:web:22930404d45ac4430d823b",
-  measurementId: "G-ZNJHLQSDEY"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
